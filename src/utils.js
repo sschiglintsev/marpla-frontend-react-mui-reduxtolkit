3604dcc8b6cc6af98cc19c219e3cd42405122477
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-10-20 02:47:31 +0500

    add: function filterRows for filter rows

--- src/utils.js
+++ src/utils.js
@@ -26,3 +26,35 @@ export const getQueryParams = () => ({
   WBToken: getLocalStorageItem("WBToken"),
   "x-supplier-id-external": getLocalStorageItem("x-supplier-id-external"),
 });
+
+export const filterRows = (rows,filterStatus) => {
+    let newRows=[];
+    if (!filterStatus) {filterStatus=''}
+
+    switch (filterStatus) {
+        case "total": {
+            newRows = rows
+            break;
+        }
+        case "active": {
+            newRows = rows.filter((el)=>{
+                return el.statusId==="Активна"
+            });
+            break;
+        }
+        case "pause": {
+            newRows = rows.filter((el)=>{
+                return el.statusId==="Приостановлено"
+            });
+            break;
+        }
+        case "archive": {
+            newRows = rows.filter((el) => {
+                return el.statusId === "Показы завершены"
+            });
+            break;
+        }
+    }
+
+    return newRows
+}
